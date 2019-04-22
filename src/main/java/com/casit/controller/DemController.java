f0db9c6af7c4d57564ab6e8ba115c23568ee15e6
GStang <420526316@qq.com>
2019-04-22 17:22:37 +0800

    Initcommit

---
+++ src/main/java/com/casit/controller/DemController.java
@@ -0,0 +1,11 @@
+package com.casit.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by DELL on 2019/4/22.
+ */
+@Controller
+public class DemController {
+
+}
---
+++ src/main/java/com/casit/service/DemoController.java
@@ -0,0 +1,10 @@
+package com.casit.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by DELL on 2019/4/22.
+ */
+@Service
+public class DemoController {
+}
