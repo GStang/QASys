09a41c536d81ba4717839f7f6c0206ee1dd29205
GStang <420526316@qq.com>
2019-04-28 14:37:56 +0800

    'classifier'

---
+++ src/main/java/com/swpuiot/qaservice/Classifier.java
@@ -0,0 +1,7 @@
+package com.swpuiot.service;
+
+/**
+ * Created by DELL on 2019/4/28.
+ */
+public class Classifier {
+}
---
+++ src/main/java/com/swpuiot/qaservice/QuestionParser.java
@@ -0,0 +1,14 @@
+package com.swpuiot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by DELL on 2019/4/28.
+ */
+public class QuestionParser {
+    public static Map buildEntityDict() {
+        Map entityDict = new HashMap();
+        
+    }
+}
