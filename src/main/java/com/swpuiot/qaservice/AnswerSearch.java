ba62bf209909892dec8d96f4d512d2fea3ddf97e
GStang <420526316@qq.com>
2019-04-30 20:53:24 +0800

    commit AnswerSearch

---
+++ src/main/java/com/swpuiot/qaservice/AnswerSearch.java
@@ -0,0 +1,20 @@
+package com.swpuiot.qaservice;
+
+import java.util.List;
+
+/**
+ * Created by DELL on 2019/4/30.
+ */
+public class AnswerSearch {
+//    执行cypher查询，并返回相应结果
+    public static void search_main(List<String> sqls) {
+        List finalAnswers;
+        for (String sql : sqls) {
+
+        }
+    }
+//根据对应的questionType，调用相应的回复模板
+    public static void answer_prettify(String questionType, String answers) {
+
+    }
+}
